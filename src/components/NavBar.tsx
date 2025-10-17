d84cd394433926f733b581b3346049dc76abc8f1
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-10-17 15:23:22 +0530

    feat: refactor NavBar for cleaner syntax and improved layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,23 +8,26 @@ export default function NavBar() {
   ];
 
   return (
-    <nav className="absolute h-[90px] top-[34px] left-[30px] right-[30px] flex items-center justify-between px-4 py-2 bg-[#FAFAFA] text-gray-800 rounded-[70px]">
-      <div className="flex flex-row items-center space-x-4">
+    <nav className="absolute h-[90px] top-[34px] left-[30px] right-[30px] flex items-center justify-between px-8 py-2 bg-[#FAFAFA] text-gray-800 rounded-[70px]">
+      <div className="flex items-center">
         <img src={'img_6.png'} alt="logo" className="w-[93.89px] h-[27.5px]" />
-        <div className="flex flex-row items-center gap-6 px-4 py-2 bg-[#FFFFFF] text-gray-800 rounded-[40px] h-[60px]">
-          {navItems.map((item, index) => (
-            <a
-              key={index}
-              className="text-[#1F1F1F] font-medium"
-              href={item.href}
-            >
-              {item.label}
-            </a>
-          ))}
-          <img src={'img_7.png'} alt={'search'} className="w-[24px] h-[24px]" />
-        </div>
-        <div className="text-[#1F1F1F] font-medium">Other services</div>
-        <div className="flex items-center space-x-4 rounded-[40px] h-[60px] w-[158px] border-[1px] gap-[10px] py-[15px] px-[35px]">
+      </div>
+
+      <div className="flex items-center gap-6 px-4 py-2 bg-[#FFFFFF] text-gray-800 rounded-[40px] h-[60px]">
+        {navItems.map((item, index) => (
+          <a
+            key={index}
+            className="text-[#1F1F1F] font-medium"
+            href={item.href}
+          >
+            {item.label}
+          </a>
+        ))}
+        <img src={'img_7.png'} alt={'search'} className="w-[24px] h-[24px]" />
+      </div>
+      <div className="text-[#1F1F1F] font-medium">Other services</div>
+      <div className="flex items-center">
+        <div className="flex items-center rounded-[40px] h-[60px] w-[158px] border-[1px] py-[15px] px-[35px]">
           Contact us
         </div>
       </div>
