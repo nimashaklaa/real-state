5a5a73bc62b87c7baab9b7b9e16977815e50587c
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-10-17 22:48:13 +0530

    feat: Add AboutUs component to enhance content in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar.tsx';
 import HeroSection from './components/HeroSection.tsx';
+import AboutUs from './components/AboutUs.tsx';
 
 function App() {
   return (
     <div className="min-h-screen bg-[#FFFFFF]">
       <NavBar />
       <HeroSection />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,51 @@
+export default function AboutUs() {
+  const steps = [
+    {
+      icon: 'img_1.png',
+      alt: 'chat',
+      title: 'Answer questions',
+      description:
+        'Lorem ipsum dolor sit amet consectetur. Adipiscing imperdiet bibendum in in vestibulum.',
+    },
+    {
+      icon: 'img_2.png',
+      alt: 'message',
+      title: 'Select a quote',
+      description:
+        'Lorem ipsum dolor sit amet consectetur. Adipiscing imperdiet bibendum in in vestibulum.',
+    },
+    {
+      icon: 'img_3.png',
+      alt: 'register',
+      title: 'Get registered',
+      description:
+        'Lorem ipsum dolor sit amet consectetur. Adipiscing imperdiet bibendum in in vestibulum.',
+    },
+  ];
+
+  return (
+    <div className="relative flex flex-col mt-[90px] mx-8 mb-[132px] items-center">
+      <div className="text-base font-normal">Three steps. Three minutes</div>
+      <div className="font-semibold text-[50px] mb-[45px]">
+        Everything should be this easy.
+      </div>
+      <div className="flex flex-row items-center justify-between w-full">
+        {steps.map((step, index) => (
+          <div key={index} className="w-[329px]">
+            <img
+              src={step.icon}
+              alt={step.alt}
+              className="h-[75px] w-[75px] mb-[22px]"
+            />
+            <div className="font-semibold text-[28px] mb-[22px] leading-1.2">
+              {step.title}
+            </div>
+            <div className="font-normal text-base leading-6">
+              {step.description}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
