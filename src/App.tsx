2455959ad1addbd823f242c043a438a9b71da1db
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-10-17 15:10:43 +0530

    feat: refactor code for cleaner syntax and add Prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import NavBar from "./components/NavBar.tsx";
+import NavBar from './components/NavBar.tsx';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#FFFFFF]">
+    <div className="min-h-screen bg-[#FFFFFF] relative">
+      <NavBar />
       <div className="container mx-auto px-4 py-8">
         <div className="max-w-4xl mx-auto">
-          <header className="text-center mb-12">
-            <NavBar/>
-          </header>
+          <header className="text-center mb-12"></header>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
